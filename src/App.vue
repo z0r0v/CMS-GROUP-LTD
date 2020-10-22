411d235991ec1105a5c30471205dc6d8d054df3a
Your Name <3767082@mail.ru>
2020-10-22 18:13:15 +0300

    uploaded cart logick and style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
     <div id="app">
         <ProductComponent></ProductComponent>
-        <CartComponent></CartComponent>
     </div>
 </template>
 
 <script>
 
 import ProductComponent from './components/ProductComponent.vue'
-import CartComponent from "./components/CartComponent";
-
 export default {
   name: 'App',
   components: {
       ProductComponent,
-      CartComponent
-  }
+  },
 }
 </script>
 
 <style>
 #app {
     display: flex;
+    flex-wrap: wrap;
     width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,7 +27,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
     .no-display {
         display: none;
     }
+
+
 </style>
--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -1,24 +1,5 @@
 <template>
-    <div class="cart">
-        <div class="popup">
-            <h3></h3>
-        </div>
-        <table>
-            <tr>
-                <th class="name">{{nameText}}</th>
-                <th>{{qtyText}}</th>
-                <th>{{priceText}}</th>
-                <th></th>
-            </tr>
-            <tr>
-                <td>Ячейка 3</td>
-                <td>Ячейка 4</td>
-                <td>Ячейка 4</td>
-                <td>{{delText}}</td>
-            </tr>
-        </table>
 
-    </div>
 </template>
 
 <script>
@@ -26,36 +7,13 @@
         name: "CartComponent",
         data() {
             return {
-                nameText:'Наименование товара и описание',
-                priceText: 'Цена',
-                qtyText:'Колличество',
-                delText:'Удалить',
+
             }
         },
     }
 
-    document.addEventListener("DOMContentLoaded", () => {
-        document.querySelectorAll('.item').forEach(elem=>elem.addEventListener('click', function () {
-            console.log(123)
-            // let name = this.querySelectorAll('h5')[0].innerText;
-            // let id = this.dataset.id;
-            // this.data.arrayCart.push({name, id});
-        }));
-    });
 </script>
 
 <style lang="scss">
-    .cart {
-        th, td {
-            text-align: left;
-        }
-        width: 50%;
-        table {
-            font-size: 12px;
 
-        }
-        .name {
-            width: 60%;
-        }
-    }
 </style>
